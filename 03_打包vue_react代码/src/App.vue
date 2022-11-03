31391b89fa9c2ad3f81e0c2e9d0f33ea88ff6283
mufeiyu-ayu <2382839439@qq.com>
2022-11-03 23:52:21 +0800

    nothing

---
+++ 03_打包vue_react代码/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div class="container">
+
+        <h1>打包速度</h1>
+
+        <h1 class="a1">{{title}}</h1>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: '打包vue2代码'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.container {
+    width: 300px;
+    height: 300px;
+    background-color: red;
+    .a1 {
+        color: blue;
+    }
+}
+</style>
+
---
+++ 03_打包vue_react代码/src/index.js
@@ -0,0 +1,5 @@
+import Vue from 'vue/dist/vue.esm'
+import App from './App.vue'
+new Vue({
+    render: (h) => h(App)
+}).$mount('#app')
